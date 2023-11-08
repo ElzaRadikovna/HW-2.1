52c171f447ac44d6f1225056cc091e5ef377c24d
Neto test <akmalova.elza@yandex.ru>
2023-11-08 12:30:53 +0300

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 15_000;
+        int x = 20;
+
+        int bonus = amount / x;
+
+        System.out.println("Итоговый бонус: " +bonus);
+
+    }
+}
