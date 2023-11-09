8e682a74475b8d5ec2b48dd6a015c3ba2c04d22b
Neto test <akmalova.elza@yandex.ru>
2023-11-09 20:50:48 +0300

    Main changed

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int amount = 15_000;
-        int x = 20;
+        int SettlementValue = 20;
 
-        int bonus = amount / x;
+        int bonus = amount / SettlementValue;
 
         System.out.println("Итоговый бонус: " +bonus);
 
